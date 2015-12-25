4557f5f5024513664ec5d8f08b540f4b150659f3
emixam360 <maxime.palmieri@gmail.com>
2015-12-25 15:50:57 +0100

    Ajout d'un Activity
    Lancement d'une nouvelle activity qui servira à l'upload de photos

--- ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/Activity/MainActivity.java
+++ ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.esstin.benjamin.imgurproject.Activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -51,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+                startActivity(intent);
             }
         });
 
