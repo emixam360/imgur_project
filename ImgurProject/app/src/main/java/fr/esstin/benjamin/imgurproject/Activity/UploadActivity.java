ab4c8f314c4c9a2dc03d211c1de93099be0cd9d5
emixam360 <maxime.palmieri@gmail.com>
2016-01-05 11:52:16 +0100

    Upload
    AJout Enregistrement des photos prises avec l'appareil et écriture

--- ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/Activity/UploadActivity.java
+++ ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/Activity/UploadActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -32,11 +37,15 @@ public class UploadActivity extends AppCompatActivity {
     Button button_take, button_select,button_save,button_upload, button_write;
     EditText editText;
     ImageView imageView;
+    Canvas canvas;
+
+    Bitmap bitmap;
+
 
     Boolean picture_selected = Boolean.FALSE;
 
-    private static int REQUEST_IMAGE_CAPTURE = 1;
     private static int RESULT_LOAD_IMAGE = 2;
+    private static int REQUEST_TAKE_PHOTO = 3;
 
 
     @Override
@@ -76,14 +85,33 @@ public class UploadActivity extends AppCompatActivity {
         button_take.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent i = new Intent(
-                        MediaStore.ACTION_IMAGE_CAPTURE);
-                if (i.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Ensure that there's a camera activity to handle the intent
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+                    }
                 }
+
             }
         });
 
+        button_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                write();
+            }
+        });
     };
 
     public void setPicture_selected(){
@@ -96,6 +124,59 @@ public class UploadActivity extends AppCompatActivity {
             picture_selected = Boolean.TRUE;
         };
     };
+
+    public void write(){
+        android.graphics.Bitmap.Config bitmapConfig =
+                bitmap.getConfig();
+        // set default bitmap config if none
+        if(bitmapConfig == null) {
+            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+        }
+        // resource bitmaps are imutable,
+        // so we need to convert it to mutable one
+        bitmap = bitmap.copy(bitmapConfig, true);
+        canvas = new Canvas(bitmap);
+
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.rgb(255, 255, 255));
+        // text size in pixels
+        paint.setTextSize((int) (240));
+
+        Typeface tf =Typeface.createFromAsset(getAssets(),"impact.ttf");
+        paint.setTypeface(tf);
+
+
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(editText.getText().toString(), 0, editText.getText().toString().length(), bounds);
+        int x = (bitmap.getWidth() - bounds.width())/2;
+        int y = (bitmap.getHeight() + bounds.height())/2;
+
+        canvas.drawText(editText.getText().toString(), x, y, paint);
+
+        imageView.setImageBitmap(bitmap);
+    };
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -114,15 +195,15 @@ public class UploadActivity extends AppCompatActivity {
             cursor.close();
 
             setPicture_selected();
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            bitmap = BitmapFactory.decodeFile(picturePath);
+            imageView.setImageBitmap(bitmap);
         }
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
 
             setPicture_selected();
-            imageView.setImageBitmap(imageBitmap);
+            imageView.setImageBitmap(bitmap);
 
 /*
             Toast.makeText(this, "Image enregistrée dans " + getApplicationContext().getString(R.string.foldername),
