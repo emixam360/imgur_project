026d51c988bd806e73b1322682a8fbe1b35f0562
Benjamin <vouillaume.benjamin@gmail.com>
2016-01-07 15:03:31 +0100

    Revert "It's WORKING !"
    This reverts commit 2a393abff92b6066a4dc8dddcdea693fb3dff8c4.

--- ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/imgurModel/GalleryParents.java
+++ ImgurProject/app/src/main/java/fr/esstin/benjamin/imgurproject/imgurModel/GalleryParents.java
@@ -1,11 +1,9 @@
 package fr.esstin.benjamin.imgurproject.imgurModel;
 
-import java.io.Serializable;
-
 /**
  * Created by Benjamin on 02/12/2015.
  */
-public class GalleryParents implements Serializable{
+public class GalleryParents {
 
 
 }
